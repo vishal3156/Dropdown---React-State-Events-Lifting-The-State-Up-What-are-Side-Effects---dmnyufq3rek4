e3afad067529f76068745a4172c603582b146d01
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-05-09 16:52:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -140,13 +140,80 @@ const states = [{
 
 function App() 
 {
-	// Do not alter/remove main div
+	const [state, setState] = useState(0); // 0th index element (it can be 0,1,2,3)
+	const [city, setCity] = useState(0);
+	const [landmark, setLandmark] = useState(0);
+
+	const getOptionArray = (arr) => {
+	let options = arr.map((place, index) => {
+	return <option key={index} value={index}>{arr[index].name}</option>
+	})
+
+	return options;
+	}
+
+	const getDivs = (place, obj) => {
+	return (
+	<div className="divStyles">
+	<div id={`${place}-name`}>
+	<strong>{obj.name}</strong>
+	</div>
+	<div id={`${place}-title`}>
+	<strong>{obj.name}</strong>
+	</div>
+	<div id={`${place}-description`}>
+	{obj.description}
+	</div>
+	</div>
+	)
+	}
+
+	const handleStateChange = (event) => {
+	setState(event.target.value);
+	}
+
+	const handleCityChange = (event) => {
+	setCity(event.target.value);
+	}
+
+	const handleLandmarkChange = (event) => {
+	setLandmark(event.target.value);
+	}
+
 	return (
 	<div id="main">
-		
+	<div id="selectContainer">
+	<select id="state" value={state} onChange={handleStateChange} className = "selectStyles">
+	{getOptionArray(states)}
+	</select>
+
+	<select id="city" value={city} onChange={handleCityChange} className = "selectStyles">
+	{getOptionArray(states[state].city)}
+	</select>
+
+	<select id="landmark" value={landmark} onChange={handleLandmarkChange} className = "selectStyles">
+	{getOptionArray(states[state].city[city].landmarks)}
+	</select>
+	</div>
+
+
+	<div id="divContainer">
+	
+	{getDivs("state", states[state])}
+	
+
+	
+	{getDivs("city", states[state].city[city])}
+	
+
+	
+	{getDivs("landmark", states[state].city[city].landmarks[landmark])}
+	
+	</div>
+	
+
 	</div>
 	);
 }
-
 
 export default App;
